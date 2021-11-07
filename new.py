eff73ada59576327796c1d83aa7374a78aaac65e
Anahita Hassan <anahitarhassan@gmail.com>
2021-11-07 01:28:40 -0400

    new

--- new.py
+++ new.py
@@ -2,41 +2,60 @@ import json
 with open("db.json") as f:
     db = json.loads(json.load(f))
 
-# for elem in db['15122']:
-#     print(elem)
+monday21127Lectures = []
+monday21127Sections = []
 
-print(db['21127'])
+listOfClassCodes = ['21127', '15122']
+
+for classCode in listOfClassCodes:
+    lecDict = db[classCode]['lec']
+    if 'M' in lecDict['day']:
+        timeDict = lecDict['time']
+        for key in timeDict:
+            miniTuple = (key, timeDict[key])
+            monday21127Lectures.append(miniTuple)
+        print(monday21127Lectures)
+
+    secDict = db[classCode]['sec']
+    if 'M' in secDict['day']:
+        timeDict = secDict['time']
+        for key in timeDict:
+            miniTuple = (key, timeDict[key])
+            monday21127Sections.append(miniTuple)
+        print(monday21127Sections)
+
+# print(db['15122'])
+# print(db['21127'])
 
 # {
-# 'lec': 
-#     { 
-#       'day': ['TR'], 
-#       'time': 
-#         { 'Lec 1': [515, 595], 
-#           'Lec 2': [610, 690]
-#         }
-#     }, 
-# 'sec': 
-#     { 'day': ['F'], 
-#       'time': 
-#         { 'A': [545, 595], 
-#         '\xa0': [1000, 1050], 
-#         'B': [610, 660], 
-#         'C': [675, 1445], 
-#         'D': [1460, 790], 
-#         'E': [805, 855], 
-#         'F': [870, 920], 
-#         'G': [935, 985], 
-#         'H': [1000, 1050], 
-#         'I': [545, 595], 
-#         'J': [610, 660], 
-#         'K': [675, 1445], 
-#         'L': [1460, 790], 
-#         'M': [805, 855], 
-#         'N': [870, 920], 
-#         'O': [935, 985], 
-#         'P': [1000, 1050]
-#         }
-#     }, 
-# 'title': 'Principles of Imperative Computation'
+#     'lec': 
+#         {
+#             'day': ['M', 'F', 'W'], 
+#             'time':     
+#                 {
+#                     'Lec 1': [545, 595], 
+#                     'Lec 2': [675, 725], 
+#                     'Lec 3': [870, 920]
+#                 }
+#         }, 
+#     'sec': 
+#         {
+#             'day': ['TR'], 
+#             'time': 
+#                 {
+#                     'A': [545, 595], 
+#                     'B': [610, 660], 
+#                     'C': [740, 790], 
+#                     'D': [870, 920], 
+#                     'E': [610, 660], 
+#                     'F': [740, 790], 
+#                     'G': [805, 855], 
+#                     'H': [935, 985], 
+#                     'I': [740, 790], 
+#                     'J': [805, 855], 
+#                     'K': [870, 920], 
+#                     'L': [935, 985]
+#                 }
+#         }, 
+#     'title': 'Concepts of Mathematics'
 # }
